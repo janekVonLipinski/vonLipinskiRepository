e84340de8a827182c9268c7647588bb8d362665d
janekVonLipinski <janekvonlipinski222@gmail.com>
2023-10-16 14:31:02 +0200

    Abstände sind nun gleichmäßig

--- EinMalEins.java
+++ EinMalEins.java
@@ -4,7 +4,7 @@ public class EinMalEins {
 
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                System.out.print(i * j + " ");
+                System.out.printf("%-3d ", i * j);
             }
             System.out.println();
         }
